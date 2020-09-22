be8dc76bdb2faada1afcf1bd34c9b07a87d9c62a
lpdft <772346730@qq.com>
2020-09-22 19:37:08 +0800

    [ruyue] feat:add cross line

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import '../styles/introduction.css'
+import '../styles/introduction.scss'
 
 const Introduction = () => {
   return (
     <div className="introduction">
       <h1>Hello,</h1>
       <h2>MY NAME IS KAMIL 24YO AND THIS IS MY RESUME/CV</h2>
+      <hr className="hrline"/>
     </div>
   );
   

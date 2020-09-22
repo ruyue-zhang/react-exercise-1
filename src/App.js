6d64a6b1914fe3a352e25ec79f40fe1844c2f24a
lpdft <772346730@qq.com>
2020-09-22 18:58:31 +0800

    [ruyue] feat:implement personal introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Avatar from './components/avatar';
+import Introduction from './components/introduction';
 
 class App extends Component {
   render() {
     return <main className="app">
              <Avatar />
+             <Introduction />
            </main>;
   }
 }
---
+++ src/components/introduction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/introduction.css'
+
+const Introduction = () => {
+  return (
+    <div className="introduction">
+      <h1>Hello,</h1>
+      <h2>MY NAME IS KAMIL 24YO AND THIS IS MY RESUME/CV</h2>
+    </div>
+  );
+  
+}
+
+export default Introduction;

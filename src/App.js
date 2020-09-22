59de045329b7fb8cf86e8f60df8920ee91ea3a22
lpdft <772346730@qq.com>
2020-09-22 19:58:32 +0800

    [ruyue] feat:implement about me component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.scss';
 import Avatar from './components/avatar';
 import Introduction from './components/introduction';
+import About from './components/about';
 
 class App extends Component {
   render() {
     return <main className="app">
              <Avatar />
              <Introduction />
+             <About />
            </main>;
   }
 }
---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/about.scss'
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>ABOUT ME</h1>
+      <p>
+        Lorem ipsum dolor sit amet,consectetur adipisicing
+        elit.Repellendus,non,dolorem,cumque distinctio magni quam expedita velit
+        laborum sunt amet facere tempora ut fuga aliquam ad asperiores voluptatem
+        dolorum!Quasi
+      </p>
+    </div>
+  );
+  
+}
+
+export default About;
